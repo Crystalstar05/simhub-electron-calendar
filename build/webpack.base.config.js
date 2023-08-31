ae8853b55fb358f294a9848a8db32d9da61e1c0f
Crystalstar05 <54647199+Crystalstar05@users.noreply.github.com>
2023-08-31 14:35:23 -0700

    Set-up
    - updated versions
    - made it so that npm start works, but not all of the features work

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
-const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 
 const translateEnvToMode = (env) => {
   if (env === "production") {
@@ -37,8 +36,6 @@ module.exports = env => {
         }
       ]
     },
-    plugins: [
-      new FriendlyErrorsWebpackPlugin({ clearConsole: env === "development" })
-    ]
+    plugins: []
   };
 };
